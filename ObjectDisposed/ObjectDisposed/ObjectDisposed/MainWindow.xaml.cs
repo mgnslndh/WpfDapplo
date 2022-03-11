1d64bacfafe0b1ede02e3b2ee066aa027e8f2e89
Magnus Lindhe <magnus.lindhe@emway.se>
2022-03-11 16:04:17 +0100

    ObjectDisposedException on ctrl-c from command prompt

---
+++ ObjectDisposed/ObjectDisposed/ObjectDisposed/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Dapplo.Microsoft.Extensions.Hosting.Wpf;
+using System.Windows;
+
+namespace ObjectDisposed
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IWpfShell
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ObjectDisposed/ObjectDisposed/ObjectDisposed/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Dapplo.Microsoft.Extensions.Hosting.AppServices;
+using Dapplo.Microsoft.Extensions.Hosting.Wpf;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ObjectDisposed
+{
+    public static class Program
+    {
+        public static Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureLogging()
+                .ConfigureWpf(wpfBuilder => {
+                    wpfBuilder.UseApplication<App>();
+                    wpfBuilder.UseWindow<MainWindow>();
+                })
+                .UseWpfLifetime()
+                .UseConsoleLifetime()
+                .Build();
+
+            Console.WriteLine("Run!");
+
+            return host.RunAsync();
+        }
+
+        /// <summary>
+        /// Configure the loggers
+        /// </summary>
+        /// <param name="hostBuilder">IHostBuilder</param>
+        /// <returns>IHostBuilder</returns>
+        private static IHostBuilder ConfigureLogging(this IHostBuilder hostBuilder) =>
+            hostBuilder.ConfigureLogging((hostContext, configLogging) =>
+            {
+                configLogging
+                    .AddConfiguration(hostContext.Configuration.GetSection("Logging"))
+                    .AddConsole()
+                    .AddDebug();
+            });
+    }
+}
